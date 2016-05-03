393b1905c8ab085444116c2fb3a3d84155ea5fce
Andrew Shaffer <andrew@weo.io>
2016-05-03 12:57:12 -0700

    wrap all other components

--- src/index.js
+++ src/index.js
@@ -3,17 +3,40 @@
  */
 
 import CSSContainer from './CSSContainer'
+import element from 'vdux/element'
 import MenuItem from './MenuItem'
 import Dropdown from './Dropdown'
 import Button from './Button'
 import Input from './Input'
 
 /**
+ * Wrap
+ */
+
+function wrap (components) {
+  const result = {}
+
+  for (let key in components) {
+    const Component = components[key]
+
+    result[key] = function render ({props, children}) {
+      return (
+        <CSSContainer ui={Component} {...props}>
+          {children}
+        </CSSContainer>
+      )
+    }
+  }
+
+  return result
+}
+
+/**
  * Exports
  */
 
 module.exports = {
-  ...require('vdux-ui'),
+  ...wrap(require('vdux-ui')),
   CSSContainer,
   Button,
   MenuItem,
