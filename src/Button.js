27fedcaed3093d9d2fffd0318c8dea6fee4691ed
Michael Weintraub <mweint@gmail.com>
2016-05-05 19:36:20 -0700

    added active ans focus props to buttons

--- src/Button.js
+++ src/Button.js
@@ -14,7 +14,7 @@ function render ({props, children}) {
   const {ui = Button} = props
 
   return (
-    <CSSContainer hoverProps={{highlight: true}} lingerProps={{ttShown: true}} {...props} ui={ui}>
+    <CSSContainer hoverProps={{highlight: true}} focusProps={{highlight: true}} activeProps={{highlight: false}} lingerProps={{ttShown: true}} {...props} ui={ui}>
       {children}
     </CSSContainer>
   )
