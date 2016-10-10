3195d8945b6193adbc76daf2104960a6df07c0bb
Andrew Shaffer <andrew@weo.io>
2016-10-10 16:34:25 -0700

    more performant textarea autosizing

--- src/Textarea.js
+++ src/Textarea.js
@@ -8,26 +8,55 @@ import CSSContainer from './CSSContainer'
 import {findDOMNode} from 'virtex'
 import element from 'vdux/element'
 import {Textarea} from 'vdux-ui'
-import autosize from 'autosize'
 import wrap from './wrap'
+
+/**
+ * Hidden Textarea
+ */
+
+let hiddenTextarea
+const hiddenTextareaStyle = 'position: absolute !important; visibility: hidden !important; overflow: hidden !important; min-height: 0px !important; max-height: 0px !important; height: 0px !important; z-index: -1000 !important; right: 0px !important; top: 0px !important'
+
+/**
+ * Sizing-related styles
+ */
+
+const sizingStyles = [
+  'letter-spacing',
+  'line-height',
+  'padding-top',
+  'padding-bottom',
+  'font-family',
+  'font-weight',
+  'font-size',
+  'text-rendering',
+  'text-transform',
+  'width',
+  'text-indent',
+  'padding-left',
+  'padding-right',
+  'border-width',
+  'box-sizing'
+]
 
 /**
  * onCreate
  */
 
 function onCreate (model) {
-  return () => {
-    if (typeof window === 'undefined') return
-
+  return dispatch => {
     setTimeout(() => {
-      const ta = findDOMNode(model).querySelector('textarea')
-      if (ta) {
-        autosize(ta)
-        const e = document.createEvent('event')
-        e.initEvent('autosize:update', true, false)
-        ta.dispatchEvent(e)
+      const node = findDOMNode(model)
+
+      if (node) {
+        const ta = node.querySelector('textarea')
+
+        if (ta) {
+          const newHeight = calculateHeight(ta)
+          dispatch(model.local(setHeight)(newHeight))
+        }
       }
-    }, 100)
+    })
   }
 }
 
@@ -36,12 +65,12 @@ function onCreate (model) {
  */
 
 function render ({props, state, local}) {
-  const {h, sq, height, onChange, onInvalid} = props
+  const {onChange, onInvalid, onInput} = props
   const {invalid, message} = state
 
-  if (h || sq || height) {
-    throw new Error('<Textarea/>: autogrowable textarea does not accept an explicit height. If you want to set a starting height parameter, uses the `rows` prop')
-  }
+  const height = isNaN(props.height)
+    ? state.height
+    : Math.max(Number(props.height), state.height)
 
   return (
     <Textarea
@@ -49,16 +78,32 @@ function render ({props, state, local}) {
       message={message}
       rows={1}
       {...props}
+      resize='none'
+      overflow='hidden'
+      onInput={[handleInput, onInput]}
       onChange={[onChange, local(e => setValidity(''))]}
       onInvalid={[onInvalid, local(e => setValidity(e.target.validationMessage))]}/>
   )
+
+  function handleInput (e) {
+    const newHeight = calculateHeight(e.target)
+
+    if (newHeight !== state.height) {
+      e.target.style.height = isNaN(props.height)
+        ? newHeight + 'px'
+        : Math.max(Number(props.height), state.height) + 'px'
+
+      return local(setHeight)(newHeight)
+    }
+  }
 }
 
 /**
  * Actions
  */
 
-const setValidity = createAction('<Textarea/>: setValidity')
+const setValidity = createAction('<Textarea/>: set validity')
+const setHeight = createAction('<Textarea/>: set height')
 
 /**
  * Reducer
@@ -66,10 +111,52 @@ const setValidity = createAction('<Textarea/>: setValidity')
 
 const reducer = handleActions({
   [setValidity]: (state, message) => ({
+    ...state,
     invalid: !!message,
     message: message
+  }),
+  [setHeight]: (state, height) => ({
+    ...state,
+    height
   })
 })
+
+/**
+ * Helpers
+ */
+
+function calculateHeight (ta) {
+  const {boxSizing, paddingSize, borderSize, sizingStyle} = getNodeStyle(ta)
+
+  hiddenTextarea = hiddenTextarea || createHiddenTextarea()
+  hiddenTextarea.value = ta.value || ta.placeholder || 'x'
+  hiddenTextarea.setAttribute('style', sizingStyle + ';' + hiddenTextareaStyle)
+
+  const height = hiddenTextarea.scrollHeight
+  return boxSizing === 'border-box'
+    ? height + borderSize
+    : height - paddingSize
+}
+
+function getNodeStyle (node) {
+  const style = window.getComputedStyle(node)
+
+  return {
+    boxSizing: style.getPropertyValue('box-sizing') || style.getPropertyValue('-moz-box-sizing') || style.getPropertyValue('-webkit-box-sizing'),
+    paddingSize: parseFloat(style.getPropertyValue('padding-bottom')) + parseFloat(style.getPropertyValue('padding-top')),
+    borderSize: parseFloat(style.getPropertyValue('border-bottom-width')) + parseFloat(style.getPropertyValue('border-top-width')),
+    sizingStyle: sizingStyles.map(name => `${name}:${style.getPropertyValue(name)}`).join(';')
+  }
+}
+
+function createHiddenTextarea () {
+  const node = document.createElement('textarea')
+
+  node.setAttribute('style', hiddenTextareaStyle)
+  document.body.appendChild(node)
+
+  return node
+}
 
 /**
  * Exports
